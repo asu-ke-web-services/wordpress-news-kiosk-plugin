3daad146a7b68125ecde2159d7c291eec727ed2a
Nagarjuna Myla <chasethenag420@gmail.com>
2015-06-25 12:35:12 -0700

    For news content removing the unneccessary HTML markup text and making it much cleaner closes #29 #27

--- helpers/feed-helper.php
+++ helpers/feed-helper.php
@@ -185,21 +185,12 @@ class Feed_Helper {
    * @return int $words
    */
   public static function content_excerpt( $content_excerpt, $words = 50 ) {
-    $content = strip_tags( $content_excerpt );
-    if ( true === strpos( $content, 'Article source:' ) ) {
-      $content = substr_replace(
-          $content,
-          '',
-          strpos( $content, 'Article source:' )
-      );
-    }
-    if ( true === strpos( $content, 'read more' ) ) {
-      $content = substr_replace(
-          $content,
-          '',
-          strpos( $content, 'read more' )
-      );
-    }
+    $content = self::normalize_html_string( $content_excerpt );
+    $content = str_replace(
+        array( 'Article source:', 'read more' ),
+        '',
+        $content
+    );
     $content = trim( $content );
     // If we only have 1 paragraph and less than $words words, reset the content
     // to the full event content
@@ -236,7 +227,19 @@ class Feed_Helper {
 
     $tidy = tidy_parse_string( $content, $tidy_config, 'UTF8' );
     $tidy->cleanRepair();
-
     return '' . $tidy;
+  }
+  public static function normalize_html_string( $input ) {
+    // Strip HTML Tags
+    $clear = strip_tags( $input );
+    // Clean up things like &amp;
+    $clear = html_entity_decode( $clear );
+    // Strip out any url-encoded stuff
+    $clear = urldecode( $clear );
+    // Replace Multiple spaces with single space
+    $clear = preg_replace( '/\s+/', ' ', $clear );
+    // Trim the string of leading/trailing space
+    $clear = trim( $clear );
+    return $clear;
   }
 }
