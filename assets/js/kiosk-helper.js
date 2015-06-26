3cc5b46e1e44560514eaee0edba38b7da84a9278
Nagarjuna Myla <chasethenag420@gmail.com>
2015-06-25 17:05:00 -0700

    Javascript changes to get weather location name by trimming spaces

--- assets/js/kiosk-helper.js
+++ assets/js/kiosk-helper.js
@@ -105,7 +105,7 @@ $('.carousel').carousel({
  */
 var site_url          = $(location).attr("href");
 var tweets_limit      = 20;
-var $weather_location = $('.kiosk-weather__forecast__title').text();
+var $weather_location = $.trim($('.kiosk-weather__forecast__title').text());
 var tweets_url        = site_url + 'kiosk/twitter/limit/' + tweets_limit;
 var weather_url       = site_url + 'kiosk/weather/location='+ $weather_location;
 setInterval(function() {
